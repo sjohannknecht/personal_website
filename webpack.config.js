e6ef9f655380475af728768d2bfce3ac3c77c4fb
sjohannknecht <sjohannknecht@techfak.uni-bielefeld.de>
2023-12-07 16:18:23 +0100

    Initialize project and create HTML skeleton

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        assetModuleFilename: 'assets/img/[name].[ext]',
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Battleship',
+            template: "./src/index.html"
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(?:js|mjs|cjs)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', {targets: "defaults"}]
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            }
+        ]
+    }
+};
