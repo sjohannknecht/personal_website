9b7ac05ecd60ab743a0623655672b4facc866279
sjohannknecht <sjohannknecht@techfak.uni-bielefeld.de>
2023-12-09 12:19:27 +0100

    Add dropdown menu for mobile

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import "./index.css";
+
+const dropdown = document.querySelector(".nav--dropdown");
+
+document.addEventListener("click", toggleDropdown);
+
+function toggleDropdown(e) {
+    if (e.target.classList.contains("nav__button--mobile") || e.target.classList.contains("nav__icon--dropdown")) {
+        dropdown.classList.toggle("hidden");
+    } else {
+        dropdown.classList.add("hidden");
+    }
+}
